563fc2565266ba166071cf6fba8ee52746428cd5
Aleksandr Kuznetsov <sahakuz03@gmail.com>
2021-12-04 14:18:22 +0500

    Updates

---
+++ precache-manifest.60abdacc0a788d5c903ee3f15386acb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8425ab9dc0aab47d05e0",
+    "url": "./static/css/main.6ca53bc0.chunk.css"
+  },
+  {
+    "revision": "8425ab9dc0aab47d05e0",
+    "url": "./static/js/main.a5ec8ad3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1ae4d28e40302cce0f36",
+    "url": "./static/css/2.637d4a9e.chunk.css"
+  },
+  {
+    "revision": "1ae4d28e40302cce0f36",
+    "url": "./static/js/2.15623c29.chunk.js"
+  },
+  {
+    "revision": "15ccd93c776fd3050340fbdc34687e18",
+    "url": "./index.html"
+  }
+];
